b08c0ba60bce8ef372c1483f55fb637e5e3d5036
Thomas Chaplin <thomas.chaplin@tray.io>
2020-03-16 21:01:39 +0000

    Updates

---
+++ precache-manifest.2a766b313f4513421885d895f96b5c5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53578bb9ff94406ec3d5a05f834608f",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "44632672f481aa47d523",
+    "url": "/coronavirus-tracker/static/css/main.512e2be0.chunk.css"
+  },
+  {
+    "revision": "50834cf7e046b3fc3d9a",
+    "url": "/coronavirus-tracker/static/js/2.ba7aabca.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/coronavirus-tracker/static/js/2.ba7aabca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44632672f481aa47d523",
+    "url": "/coronavirus-tracker/static/js/main.6a548c3f.chunk.js"
+  },
+  {
+    "revision": "0996c01cf434172c4e37",
+    "url": "/coronavirus-tracker/static/js/runtime-main.2e83845a.js"
+  }
+]);
